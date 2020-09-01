6ee63483fae439d0600357120c780ad9b2e7b23f
Joy-wx <70579398+Joy-wx@users.noreply.github.com>
2020-09-01 19:33:44 +0800

    toy-react

---
+++ main.js
@@ -0,0 +1,11 @@
+for(i of [1,2,3]) {
+    console.log(i)
+}
+function createElement(tagName, attributes, ...children) {
+
+}
+let a = <div id='a' class='c'>
+    <div></div>
+    <div></div>
+    <div></div>
+</div>
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    entry: {
+        main: "./main.js"
+    },
+    module:{
+        rules: [{
+            test: /\.js$/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env'], // 转换旧语法
+                    plugins: [['@babel/plugin-transform-react-jsx', {pragma: "creatElement"}]] // 改变名称
+                }
+            }
+        }]
+    },
+    mode: "development",
+    optimization: {
+        minimize: false
+    }
+}
